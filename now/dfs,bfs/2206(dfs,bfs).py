e6fd3e4d6da51253279f9c11adf66236e38bfb93
chan0139 <chan4756@gmail.com>
2022-02-18 01:58:27 +0900

    02/17

--- now/dfs,bfs/2206(dfs,bfs).py
+++ now/dfs,bfs/2206(dfs,bfs).py
@@ -1,45 +1,37 @@
 from collections import deque
 
-n,m = map(int, input().split())
-
-miro = []
+n, m = map(int, input().split())
+maps = []
 for i in range(n):
-    miro.append(list(map(int, input())))
+    maps.append(list(map(int, input())))
 
-visited = [[[-1]* 2 for _ in range(m)] for _ in range(n)]
+check = [[[0] * 2 for _ in range(m)] for _ in range(n)]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
 def bfs(x,y):
-    q = deque()
-    q.append((x,y,0))
-    visited[0][0][0] = 1
+    queue = deque()
+    queue.append((x,y,0))
+    check[x][y][0] = 1
 
-    while q:
-        x,y,z = q.popleft()
-
+    while queue:
+        x,y,c = queue.popleft()
+        if x == n - 1 and y == m-1:
+            return check[x][y][c]
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if miro[nx][ny] == 0 and visited[nx][ny][z] == -1:
-                visited[nx][ny][z] = visited[x][y][z] + 1
-                q.append((nx,ny,z))
-            if z == 0 and miro[nx][ny] == 1 and visited[nx][ny][z] == -1:
-                visited[nx][ny][1] = visited[x][y][z] + 1
-                q.append((nx,ny,1))
+            if maps[nx][ny] == 0 and check[nx][ny][c] == 0:
+                check[nx][ny][c] = check[x][y][c] + 1
+                queue.append((nx, ny, c))
+            if maps[nx][ny] == 1 and c == 0:
+                check[nx][ny][1] = check[x][y][c] + 1
+                queue.append((nx, ny, 1))
 
-bfs(0,0)
-for i in range(n):
-    for j in range(m):
-        print(visited[i][j][0], end=' ')
-    print()
+    return -1
 
-print()
-for i in range(n):
-    for j in range(m):
-        print(visited[i][j][1], end=' ')
-    print()
+print(bfs(0,0))
